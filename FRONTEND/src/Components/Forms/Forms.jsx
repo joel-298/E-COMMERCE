334cac58531e7b3261b3f5e3e9e646a72314dce5
kashish-2874 <kashishbarthwal2874@gmail.com>
2024-11-01 14:20:22 +0530

    testing

--- FRONTEND/src/Components/Forms/Forms.jsx
+++ FRONTEND/src/Components/Forms/Forms.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Forms = () => {
   return (
     <div>
-      THIS IS THE FORM PAGE : a.k.a the seller page where the seller is going to see his lit of items and also where he can add a new item 
+      THIS IS THE FORM PAGE : a.k.a the seller page where the seller is going to see his lit of items and also where he can add a new item fhfhfhtf
     </div>
   )
 }
