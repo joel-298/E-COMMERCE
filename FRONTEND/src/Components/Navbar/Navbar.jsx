11256239b7b9280ee56303daaca7678ce412de62
joel-298 <joel29matt072004@gmail.com>
2024-11-01 14:21:01 +0530

    navbar

--- FRONTEND/src/Components/Navbar/Navbar.jsx
+++ FRONTEND/src/Components/Navbar/Navbar.jsx
@@ -11,12 +11,21 @@ const Navbar = () => {
   const handleAvatarClick = () => {
     navigate('/auth');
   };
-
+  const handleNav = () => {
+    const box5 = document.querySelector(`.${styles.box5}`);
+    if (window.innerWidth > 1024) {
+      box5.classList.add(styles.add);
+      box5.classList.remove(styles.remove);
+    } else {
+      box5.classList.add(styles.remove);
+      box5.classList.remove(styles.add);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.box2}>
         <div className={styles.box1}>
-          <img src="./Vector.svg" alt="nav_icon" className={styles.nav_icon}/>
+          <img src="./Vector.svg" alt="nav_icon" className={styles.nav_icon} onClick={handleNav}/>
         </div>
         <h1><img src="./Logo.png" alt="logo_image" className={styles.logo} />&nbsp;URBAN CART</h1>
       </div>
@@ -35,11 +44,11 @@ const Navbar = () => {
         <img src="./cart.svg" alt="cart" className={styles.cart} onClick={handleCartClick} />
         <img src="./noavatar.png" alt="avatar" className={styles.avatar} onClick={handleAvatarClick} />
       </div>
-      <div className={styles.box5}>
-      <div className={styles.items}>On Sale</div>
-        <div className={styles.items}>New Arrivals</div>
-        <div className={styles.items}>Brands</div>
-      </div>
+        <div className={styles.box5}>
+          <div className={styles.items_box5}>On Sale</div>
+          <div className={styles.items_box5}>New Arrivals</div>
+          <div className={styles.items_box5}>Brands</div>
+        </div>
     </div>
   );
 };
