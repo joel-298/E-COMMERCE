cf6b860165134213f9891160a27746502a9eee9f
joel-298 <joel29matt072004@gmail.com>
2024-11-22 11:17:28 +0530

    adding protected routes and jwt verification

--- FRONTEND/src/Components/Cart/Cart.jsx
+++ FRONTEND/src/Components/Cart/Cart.jsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Cart = () => {
   return (
     <div>
-      THIS IS THE CART PAGE
+      <h3>Before coming to this we are going to create a post request in user.js /user/post which will add the particular item to the cart object of user from item.js</h3>
+
+      <h1>THIS IS THE CART PAGE .....</h1> 
+      <h3>1. WE ARE GOING TO CREATE A HTML TABLE</h3> 
+      <h3>2. WE ARE GOING TO USE A POST REQUEST TO FETCH DATA FROM USER </h3>
+      <h3>3. FOR THIS WILL CREATE A NEW FILE user.js in middleware and create a route /user/cart :- which will fetch the data present in the user cart </h3>
     </div>
   )
 }
 
-export default Cart
+export default Cart ;
