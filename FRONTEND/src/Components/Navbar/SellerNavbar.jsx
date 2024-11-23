4e49d1801e146645e9f65a31711345d2bf5ccc7f
joel-298 <joel29matt072004@gmail.com>
2024-11-24 01:32:01 +0530

    seller navbar fixed

--- FRONTEND/src/Components/Navbar/SellerNavbar.jsx
+++ FRONTEND/src/Components/Navbar/SellerNavbar.jsx
@@ -5,17 +5,21 @@ import styles from './Navbar.module.css';
 
 const SellerNavbar = () => {
   useEffect(() => {
-  // change icon based on login or not
-  const getTokenData = () => JSON.parse(localStorage.getItem('login'));
-  const tokenData = getTokenData();
-  if (!tokenData || !tokenData.token) {
-    document.querySelector(`.${styles.signout}`).classList.add(styles.display_none);
-    document.querySelector(`.${styles.avatar}`).classList.remove(styles.display_none) ;
-  }
-  else{
-    document.querySelector(`.${styles.signout}`).classList.remove(styles.display_none);
-    document.querySelector(`.${styles.avatar}`).classList.add(styles.display_none) ;     
-  }
+    // change icon based on login or not
+    const tokenData = JSON.parse(localStorage.getItem('login'));
+
+    const signoutElem = document.querySelector(`.${styles.signout}`);
+    const avatarElem = document.querySelector(`.${styles.avatar}`);
+    if (signoutElem && avatarElem) {
+      if (!tokenData.token) {
+        signoutElem.classList.add(styles.display_none);
+        avatarElem.classList.remove(styles.display_none);
+      } else {
+        signoutElem.classList.remove(styles.display_none);
+        avatarElem.classList.add(styles.display_none);
+      }
+    }
+
     // Handeling screen width and side bar ..... 
     const handleResize = () => {
       const box5 = document.querySelector(`.${styles.box5}`);
@@ -85,7 +89,7 @@ const SellerNavbar = () => {
         </div>
 
         <div className={styles.box3}>
-          <div className={styles.items} onClick={() => { navigate('/seller') }}>Home</div>
+          <div className={styles.items} onClick={() => { navigate('/admin') }}>Home</div>
         </div>
 
         <div className={styles.box4}>
@@ -115,5 +119,6 @@ const SellerNavbar = () => {
     </>
   );
 };
+
 
 export default SellerNavbar;
