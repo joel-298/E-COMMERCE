1e03a48f1db4aaba0226d1a117d0b564cb845e57
kashish-2874 <kashishbarthwal2874@gmail.com>
2024-11-24 00:42:45 +0530

    add product page

--- FRONTEND/src/Components/Forms/add/Add.jsx
+++ FRONTEND/src/Components/Forms/add/Add.jsx
@@ -1,7 +1,13 @@
 import React, { useState } from 'react'
+import axios from 'axios';
+import styles from "./add.module.css"
+import SellerNavbar from '../../Navbar/SellerNavbar';
+import Footer from '../../Footer/Footer';
+
 
 const Add = () => {
   // use state
+
   const [formData,setFormData] = useState({
     name:'',originalPrice:'',discountPercent:'',companyName:'',description:'',
     availableSize:[],
@@ -10,35 +16,34 @@ const Add = () => {
   })
 
   // function to handle inputs
-  const handleChange = ((e)=>{
-    const [name,value,type,checked] = e.target;
-    if(type === 'checkbox'){
-      setFormData((prev)=>{
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+  
+    if (type === "checkbox") {
+      setFormData((prev) => {
         // If the checkbox is checked, add the value to the availableSize array.
-        if(checked){
+        if (checked) {
           return {
             ...prev,
-            availableSize:[...prev.availableSize,value],
+            availableSize: [...prev.availableSize, value],
           };
-        }
-        // If the checkbox is unchecked, remove the value from the availableSize array.
-        else{
+        } else {
+          // If the checkbox is unchecked, remove the value from the availableSize array.
           return {
             ...prev,
-            availableSize:prev.availableSize.filter((size)=> size != value),
+            availableSize: prev.availableSize.filter((size) => size !== value),
           };
         }
       });
-    }
-    else{
+    } else {
       // For other input types (like text or number), update the field directly.
-      setFormData((prev)=>({
+      setFormData((prev) => ({
         ...prev,
-        [name]:value,
-      }))
+        [name]: value,
+      }));
     }
-
-  })
+  };
+  
 
   // to submit the value in backend
   const handleSubmit = async(e)=>{
@@ -53,52 +58,118 @@ const Add = () => {
       console.log('Response',response.data);
     } catch (error) {
       console.error('error: ',error.response?.data || error.message);
-      alert('failed to save product!!! please try again.')
+      alert('failed to save product!!! please try again.');
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label> NAME: </label>
-      <input type="text" name="name" value={formData.name} onChange={handleChange} />
-      <label>ORIGINAL PRICE: </label>
-      <input type="number" name="originalPrice" value={formData.originalPrice} onChange={handleChange} />
-      <label>DISCOUNT PERCENTAGE: </label>
-      <input type="number" name="discountPercent" value={formData.discountPercent} onChange={handleChange} />
-      <label>COMPANY NAME: </label>
-      <input type="text" name="companyName" id="" value={formData.companyName} onChange={handleChange}/>
-      <label>DESCRIPTION: </label>
-      <input type="text" name="description" value={formData.description} onChange={handleChange} />
-      <label>TOTAL QUANTITY AVAILABLE: </label>
-      <input type="number" name="totalQuantityAvailable" id="" />
-      <label>AVAILABLE SIZES: </label>
-      <div>
-        {['XS','S','M','L','XL','XXL','XXXL'.map((size)=>(
-          <label>
-            <input type="checkbox" name="availableSizes"  />
-          </label>
-        ))]}
-      </div>
-      <label>GENDER: </label>
-      <div>
-        {['Male','Female']}
-      </div>
-      <label>CATEGORY: </label>
-      <input type="text" name="category" id="" />
-      <label>TYPE: </label>
-      <input type="text" name="type" id="" />
+   <>
+      <div className={styles.container}>
+        <SellerNavbar />
 
-      <label>IMAGE: </label>
-      <input type="url" name="image" value={formData.image} onChange={handleChange} />
-      <label>IMAGE2: </label>
-      <input type="url" name="image2" value={formData.image2} onChange={handleChange} />
-      <label>IMAGE3: </label>
-      <input type="url" name="image3" value={formData.image3} onChange={handleChange} />
-      <label>IMAGE4: </label>
-      <input type="url" name="image4" value={formData.image4} onChange={handleChange} />
-      <label>IMAGE5: </label>
-      <input type="url" name="image5" id="" />
-    </form>
+        <div className={styles.content}>
+          <form onSubmit={handleSubmit} className={styles.form}>
+            <div className={styles.box}>
+              <label> NAME: </label>
+              <input type="text" name="name" value={formData.name} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>ORIGINAL PRICE: </label>
+              <input type="number" name="originalPrice" value={formData.originalPrice} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>DISCOUNT PERCENTAGE: </label>
+              <input type="number" name="discountPercent" value={formData.discountPercent} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>COMPANY NAME: </label>
+              <input type="text" name="companyName" id="" value={formData.companyName} onChange={handleChange}/>
+            </div>
+            <div className={styles.box}>
+              <label>DESCRIPTION: </label>
+              <input type="text" name="description" value={formData.description} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>TOTAL QUANTITY AVAILABLE: </label>
+              <input type="number" name="totalQuantityAvailable" value={formData.totalQuantityAvailable} onChange={handleChange} />
+            </div>
+
+            <div className={styles.box}>
+              <label>AVAILABLE SIZES: </label>
+              <div className={styles.radio}>
+                {['XS','S','M','L','XL','XXL','XXXL'].map((size)=>(
+                  <label key={size}>
+                    <input type="checkbox" name="availableSize" value={size} checked={formData.availableSize.includes(size)} onChange={handleChange} />
+                    {size}
+                  </label>
+                ))}
+              </div>
+            </div>
+            
+            <div className={styles.box}>
+              <label>GENDER: </label>
+              <div className={styles.radio}>
+                {['Male','Female','kids'].map((gender)=>(
+                  <label key={gender}>
+                    <input type="radio" name="gender" value={gender} checked={formData.gender === gender} onChange={handleChange}  />
+                    {gender}
+                  </label>
+                  
+                ))}
+              </div>
+            </div>
+            <div className={styles.box}>
+              <label>CATEGORY: </label>
+              <div className={styles.radio}>
+                {['Topwear','Bottomwear','Footwear','Accessories'].map((category)=>(
+                  <label key={category}>
+                    <input type="radio" name="category" value={category} checked={formData.category === category} onChange={handleChange} />
+                    {category}
+                  </label>
+                ))}
+              </div>
+            </div>
+            
+            <div className={styles.box}>
+              <label>TYPE: </label>
+              <div className={styles.radio}>
+                {['Party','Casual','Formal','Sports'].map((type)=>(
+                  <label key={type}>
+                    <input type="radio" name="type" value={type} checked={formData.type === type} onChange={handleChange} />
+                    {type}
+                  </label>
+                ))}
+              </div>
+            </div>
+            
+            <div className={styles.box}>
+              <label>IMAGE: </label>
+              <input type="url" name="image" value={formData.image} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>IMAGE2: </label>
+              <input type="url" name="image2" value={formData.image2} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>IMAGE3: </label>
+              <input type="url" name="image3" value={formData.image3} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>IMAGE4: </label>
+              <input type="url" name="image4" value={formData.image4} onChange={handleChange} />
+            </div>
+            <div className={styles.box}>
+              <label>IMAGE5: </label>
+              <input type="url" name="image5" value={formData.image5} onChange={handleChange} />
+            </div>
+            <div className={styles.buttonBox}>
+              <button type="submit" className={styles.button}>Add</button>
+            </div>
+          </form>
+        </div>
+        < Footer />
+      </div>
+    </>
   )
 }
 
